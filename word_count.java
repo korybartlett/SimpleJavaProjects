cad832e45d8cac7138880880c91cb53188a22f11
korybartlett <korybart@gmail.com>
2017-09-21 23:55:40 -0700

    init word count practice

---
+++ word_count.java
@@ -0,0 +1,12 @@
+/*
+ * Counts the number of individual words in a string. 
+ * For added complexity read these strings in from a text file and generate a summary.
+ * */
+public class word_count {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
