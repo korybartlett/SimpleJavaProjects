4e3cf0f104e15ed8dde78d5409a6358d3f53185e
korybartlett <korybart@gmail.com>
2017-12-03 02:37:23 -0800

    Created unique word count from given string

--- word_count.java
+++ word_count.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
@@ -5,14 +6,85 @@ import java.util.Scanner;
  * For added complexity read these strings in from a text file and generate a summary.
  * */
 public class word_count {
-
+	
+	public static ArrayList<String> checkInput(String inputString){
+		ArrayList<String> uniqueWords = new ArrayList<String> ();
+		boolean notWord = false;
+		String newString = "";
+		
+		for(int i = 0; i < inputString.length(); i++){			
+			if(Character.isLetter(inputString.charAt(i)) ){
+				notWord = false;
+				newString+=inputString.charAt(i);
+			}
+			else{
+				if(notWord){
+					continue;
+				}
+				
+				if(!uniqueWords.contains(newString) ){
+					uniqueWords.add(newString);
+				}
+				newString = "";
+				notWord = true;
+			}
+			
+		}
+		
+		if(!uniqueWords.contains(newString) ){
+			uniqueWords.add(newString);
+		}
+		
+		return uniqueWords;
+	}
+	
+	public static ArrayList<String> checkInputFromFile(String inputString){
+		ArrayList<String> uniqueWords = new ArrayList<String> ();
+		boolean notWord = false;
+		String newString = "";
+		
+		for(int i = 0; i < inputString.length(); i++){			
+			if(Character.isLetter(inputString.charAt(i)) ){
+				notWord = false;
+				newString+=inputString.charAt(i);
+			}
+			else{
+				if(notWord){
+					continue;
+				}
+				
+				if(!uniqueWords.contains(newString) ){
+					uniqueWords.add(newString);
+				}
+				newString = "";
+				notWord = true;
+			}
+			
+		}
+		
+		if(!uniqueWords.contains(newString) ){
+			uniqueWords.add(newString);
+		}
+		
+		return uniqueWords;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//lets get started
-		System.out.print("Enter string: ");
-		Scanner input = new Scanner (System.in);
-		String inputString = input.nextLine().toLowerCase();
-		input.close();
+		
+		String inputString = "This is a 5 is test\t Next Line";
+		inputString = inputString.toLowerCase();
+		System.out.println("Original String: " + inputString);
+		
+		ArrayList<String> uniqueWords = checkInput(inputString);
+		
+		System.out.println("Unique word count: " + uniqueWords.size());
+		String finalWordList = "";
+		for(int i = 0; i < uniqueWords.size(); i++){
+			String uniqueWord = uniqueWords.get(i) + " ";
+			finalWordList += uniqueWord;
+		}
+		System.out.println("Unique words: " + finalWordList);
 	}
 
 }
