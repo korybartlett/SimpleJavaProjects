1b89f142810edbcdf2eace94309dfff48c6499d7
Kory Bartlett <kory.bartlett@intel.com>
2018-08-19 18:16:54 -0700

    finished problem

--- SmallestMultiple.java
+++ SmallestMultiple.java
@@ -15,7 +15,60 @@ public class SmallestMultiple {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		boolean allDivisible = false;
+		int smallestPostiveNumber = 2520;
+		do{
+			/*
+			 * 10
+			 * 2
+			 * 5
+			 * 4
+			 * 19
+			 * 9
+			 * 3		
+			 * 6
+			 * 17
+			 * 8
+			 * 13
+			 * 11
+			 * 7
+			 * */
+			smallestPostiveNumber++;
+			if(smallestPostiveNumber % 20 != 0){
+				continue;
+			}
+			if(smallestPostiveNumber % 19 != 0){
+				continue;
+			}
+			if(smallestPostiveNumber % 18 != 0){
+				continue;
+			}
+			if(smallestPostiveNumber % 17 != 0){
+				continue;
+			}
+			if(smallestPostiveNumber % 16 != 0){
+				continue;
+			}
+			if(smallestPostiveNumber % 13 != 0){
+				continue;
+			}
+			if(smallestPostiveNumber % 11 != 0){
+				continue;
+			}
+			if(smallestPostiveNumber % 7 != 0){
+				continue;
+			}
+			if(smallestPostiveNumber % 5 != 0){
+				continue;
+			}
+			if(smallestPostiveNumber % 3 != 0){
+				continue;
+			}
+			else
+				allDivisible = true;
+		}while(!allDivisible);
+		
+		System.out.println("Answer: " + smallestPostiveNumber);
 	}
 
 }
